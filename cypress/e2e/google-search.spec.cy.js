bee21fa7a1f7c86eea4977bcde1c9404b281691d
Enrique Aguirre <enrique@yourfuzzy.com>
2023-07-12 16:59:15 -0700

    Add additional validation to google search

--- cypress/e2e/google-search.spec.cy.js
+++ cypress/e2e/google-search.spec.cy.js
@@ -3,6 +3,8 @@ describe('Google search', () => {
     it('Verify search for "Dogs"', () => {
         cy.visit('https://www.google.com')
         cy.get('[name=q]').type('Dogs{enter}')
+        cy.url().should('include', '/search?q=Dogs')
+        cy.get('[name=q]').should('have.value', 'Dogs')
         cy.contains('Dog').should('exist')
     })
 
@@ -10,6 +12,8 @@ describe('Google search', () => {
     it('Verify explicit search for "Cat"', () => {
         cy.visit('https://www.google.com')
         cy.get('[name=q]').type('Cats{enter}')
+        cy.url().should('include', '/search?q=Cats')
+        cy.get('[name=q]').should('have.value', 'Cats')
         cy.get('[data-attrid=title]').then(function($e){
             expect($e).to.contain('Cat')
         })
